2685d7f5cb58056e63fb0dc03a446a012bb13ab7
tcocco20 <theococco@yahoo.com>
2022-09-28 13:35:59 -0700

    Started adding routes and Links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Header from "./components/Header/Header";
+import ProductDetail from "./components/pages/ProductDetail";
 import Products from "./components/pages/Products";
 import Welcome from "./components/pages/Welcome";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Products />
-      </Route>
+      <Header />
+      <main>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
